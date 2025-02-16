2aa4e6aafa14c27ab447a0ca68b9ae5d52b9ee0c
PEICHI <qooqoo814@gmail.com>
2025-02-16 14:29:29 +0800

    Initial Commit

---
+++ AwesomeProject/AwesomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeProjectApp.swift
+//  AwesomeProject
+//
+//  Created by Pei Chi Cheng on 2025/2/16.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwesomeProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AwesomeProject
+//
+//  Created by Pei Chi Cheng on 2025/2/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
